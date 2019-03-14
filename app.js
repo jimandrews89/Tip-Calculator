a0355e4ddbffddcd1b09d5c80d38aad882dcafdc
Jim Andrews <jimandrews89@gmail.com>
2019-03-13 22:06:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ let billInput = document.getElementById('billTotal');
 let tipPercentInput = document.getElementById('tipPercent');
 let splitInput = document.getElementById('number-of-people');
 
+// if no data has been input, these variables equal the boolean false 
 let billValue = false;
 let tipPercentValue = false;
 let splitValue = false;
 
+// Once the this input has changed, the function checks for all three input variables to be true. 
+// This happens on every input value change so the order in which the values are input does not matter.
 billInput.onchange = function() {
     billValue = parseInt(billInput.value);
     calculateTip();
 }
+
 
 tipPercentInput.onchange = function() {
     tipPercentValue = parseFloat(document.querySelector('input[name = "tip"]:checked').value);
@@ -21,6 +25,7 @@ splitInput.onchange = function() {
     calculateTip();
 }
 
+// Once all three input values are not false, the function calculates the two outputs.
 function calculateTip() {
     if (billValue && tipPercentValue && splitValue) {
         let tipTotal = billValue * tipPercentValue;
