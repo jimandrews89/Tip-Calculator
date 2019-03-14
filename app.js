dba47fadcb69308bdb8f17b3f2ef6a3d78a0ec0a
Jim Andrews <jimandrews89@gmail.com>
2019-03-13 21:53:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-document.getElementById('container').onchange = function () {
-  var bill = Number(document.getElementById('billTotal').value);
-  var tipPercent = Number(document.querySelector('input[name = "tip"]:checked').value);
-  var tipTotal = bill * tipPercent;
-  var finalTotal = tipTotal + bill;
+let billInput = document.getElementById('billTotal');
+let tipPercentInput = document.getElementById('tipPercent');
+let splitInput = document.getElementById('number-of-people');
 
-  document.getElementById('newTipTotal').innerHTML = tipTotal;
-  document.getElementById('newTotalWithTip').innerHTML = finalTotal;
+let billValue = false;
+let tipPercentValue = false;
+let splitValue = false;
 
-};
+billInput.onchange = function() {
+    billValue = parseInt(billInput.value);
+    calculateTip();
+}
+
+tipPercentInput.onchange = function() {
+    tipPercentValue = parseFloat(document.querySelector('input[name = "tip"]:checked').value);
+    calculateTip();
+}
+
+splitInput.onchange = function() {
+    splitValue = parseInt(splitInput.value);
+    calculateTip();
+}
+
+function calculateTip() {
+    if (billValue && tipPercentValue && splitValue) {
+        let tipTotal = billValue * tipPercentValue;
+        let finalTotal = (billValue + tipTotal) / splitValue;
+
+        document.getElementById('newTipTotal').innerHTML = tipTotal;
+        document.getElementById('newTotalWithTip').innerHTML = finalTotal;
+    }
+}
+
